8eb5118311dd38dbb658187d2aadcd05d437b284
AhmedMushtaq-GitHub <you@example.com>
2023-04-26 23:49:19 -0700

    Add project files.

---
+++ Ahmed Mushtaq C# code/Program.cs
@@ -0,0 +1,61 @@
+﻿//C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.
+//C# is used to develop web apps, desktop apps, mobile apps, games and much more.
+
+
+Console.WriteLine("Write line is a block level string value");
+Console.Write("Write is a inline string value ");
+Console.WriteLine("Write is a inline string value ");
+
+
+Console.WriteLine("Hellow World");
+Console.ReadLine();//The only difference is that it does not insert a new line at the end of the output:
+Console.WriteLine("Hello World! ");
+Console.WriteLine("I will print on the same line.");
+// This is a comment
+Console.WriteLine("Hello World!");
+/* The code below will print the words Hello World
+to the screen, and it is amazing */
+Console.WriteLine("Hello World!");
+/*Variables are containers for storing data values.
+
+In C#, there are different types of variables (defined with different keywords), for example:
+
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+string - stores text, such as "Hello World". String values are surrounded by double quotes
+bool - stores values with two states: true or false*/
+
+int myNum = 5;
+double myDoubleNum = 5.99D;
+char myLetter = 'D';
+bool myBool = true;
+string myText = "Hello";
+
+
+
+
+
+
+//This will declare the variable as "constant", which means unchangeable and read-only:
+
+const int myNums = 15;
+myNum = 20; // error
+
+string firstName = "John ";
+string lastName = "Doe";
+string fullName = firstName + lastName;
+Console.WriteLine(fullName);
+//To declare more than one variable of the same type, use a comma-separated list:
+int x = 5, y = 6, z = 50;
+Console.WriteLine(x + y + z);
+int x, y, z;
+x = y = z = 50;
+Console.WriteLine(x + y + z);
+
+
+
+
+
+
+Console.WriteLine();
