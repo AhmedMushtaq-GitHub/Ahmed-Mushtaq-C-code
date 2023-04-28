1374ab93eb52d70329a439ee113bfac0597d8198
AhmedMushtaq-GitHub <you@example.com>
2023-04-28 04:15:55 -0700

    updated at 28 april

--- Ahmed Mushtaq C# code/Program.cs
+++ Ahmed Mushtaq C# code/Program.cs
@@ -1,12 +1,12 @@
 ﻿//C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.
 //C# is used to develop web apps, desktop apps, mobile apps, games and much more.
-
-
 Console.WriteLine("Write line is a block level string value");
 Console.Write("Write is a inline string value ");
 Console.WriteLine("Write is a inline string value ");
-
-
+Console.WriteLine("Hellow World");
+Console.WriteLine("Write line is a block level string value");
+Console.Write("Write is a inline string value ");
+Console.WriteLine("Write is a inline string value ");
 Console.WriteLine("Hellow World");
 Console.ReadLine();//The only difference is that it does not insert a new line at the end of the output:
 Console.WriteLine("Hello World! ");
@@ -17,15 +17,12 @@ Console.WriteLine("Hello World!");
 to the screen, and it is amazing */
 Console.WriteLine("Hello World!");
 /*Variables are containers for storing data values.
-
 In C#, there are different types of variables (defined with different keywords), for example:
-
 int - stores integers (whole numbers), without decimals, such as 123 or -123
 double - stores floating point numbers, with decimals, such as 19.99 or -19.99
 char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
 string - stores text, such as "Hello World". String values are surrounded by double quotes
 bool - stores values with two states: true or false*/
-
 int myNum = 5;
 Console.WriteLine(myNum);
 Console.ReadLine();
@@ -33,17 +30,9 @@ double myDoubleNum = 5.99D;
 char myLetter = 'D';
 bool myBool = true;
 string myText = "Hello";
-
-
-
-
-
-
 //This will declare the variable as "constant", which means unchangeable and read-only:
-
 const int myNums = 15;
 myNum = 20; // error
-
 string firstName = "John ";
 string lastName = "Doe";
 string fullName = firstName + lastName;
@@ -54,10 +43,122 @@ Console.WriteLine(x + y + z);
 int a, b, c;
 x = y = z = 50;
 Console.WriteLine(x + y + z);
-
-
-
-
-
-
 Console.WriteLine();
+//Arithmetic Operators
+Console.Write("10 + 9 = ");
+Console.WriteLine(10 + 9); //Plus Operator
+Console.Write("10 - 9 = ");
+Console.WriteLine(10 - 9); //Minus Operator
+Console.Write("10 x 9 = ");
+Console.WriteLine(10 * 9); //Multiply Operator
+Console.Write("10 / 9 = ");
+Console.WriteLine(10 / 9); //Devide Operator
+Console.Write("10 % 9 = ");
+Console.WriteLine(10 % 9); //Modules Operator
+//
+Console.Write("10 + 9 = ");
+Console.WriteLine(10 + 9); //Plus Operator
+Console.Write("10 - 9 = ");
+Console.WriteLine(10 - 9); //Minus Operator
+Console.Write("10 x 9 = ");
+Console.WriteLine(10 * 9); //Multiply Operator
+Console.Write("10 / 9 = ");
+Console.WriteLine(10 / 9); //Devide Operator
+Console.Write("10 % 9 = ");
+Console.WriteLine(10 % 9); //Modules Operator
+//
+Console.Write("10 + 9 = ");
+Console.WriteLine(10 + 9); //Plus Operator
+Console.Write("10 - 9 = ");
+Console.WriteLine(10 - 9); //Minus Operator
+Console.Write("10 x 9 = ");
+Console.WriteLine(10 * 9); //Multiply Operator
+Console.Write("10 / 9 = ");
+Console.WriteLine(10 / 9); //Devide Operator
+Console.Write("10 % 9 = ");
+Console.WriteLine(10 % 9); //Modules Operator
+//
+Console.Write("10 + 9 = ");
+Console.WriteLine(10 + 9); //Plus Operator
+Console.Write("10 - 9 = ");
+Console.WriteLine(10 - 9); //Minus Operator
+Console.Write("10 x 9 = ");
+Console.WriteLine(10 * 9); //Multiply Operator
+Console.Write("10 / 9 = ");
+Console.WriteLine(10 / 9); //Devide Operator
+Console.Write("10 % 9 = ");
+Console.WriteLine(10 % 9); //Modules Operator
+//"AND"   or  "OR" gate //Logical Operators
+
+Console.WriteLine(534<=234 && 545>=5342);
+Console.WriteLine(4234<=4325 || 435>543);
+Console.WriteLine(645 + 5435);
+Console.WriteLine(42<453 || 43>543);
+Console.ReadLine();
+//
+Console.WriteLine(534 <= 234 && 545 >= 5342);
+Console.WriteLine(4234 <= 4325 || 435 > 543);
+Console.WriteLine(645 + 5435);
+Console.WriteLine(42 < 453 || 43 > 543);
+Console.ReadLine();
+//
+Console.WriteLine(534 <= 234 && 545 >= 5342);
+Console.WriteLine(4234 <= 4325 || 435 > 543);
+Console.WriteLine(645 + 5435);
+Console.WriteLine(42 < 453 || 43 > 543);
+Console.ReadLine();
+//
+Console.WriteLine(534 <= 234 && 545 >= 5342);
+Console.WriteLine(4234 <= 4325 || 435 > 543);
+Console.WriteLine(645 + 5435);
+Console.WriteLine(42 < 453 || 43 > 543);
+Console.ReadLine();
+//Comparison Operators
+//This will Give's Answer in Boolean
+Console.WriteLine(10 > 9); //Means: 10 Greater Than 9
+Console.WriteLine(10 < 9); //Means: 10 Smaller Than 9
+Console.WriteLine(10 == 9); //Means: 10 Equals To 9
+Console.WriteLine(10 >= 9); //Means: 10 Greater Than Or Equal To 9
+Console.WriteLine(10 <= 9); //Means: 10 Smaller Than Or Equal To 9
+Console.WriteLine(10 != 9); //Means: 10 Not Equal To 9
+//Bitwise, Variables and Constants
+Console.Write("3543 | 534 Bitwise is=");
+Console.WriteLine(3543 | 534);
+Console.ReadLine();
+Console.Write("54 & 534 bitwise is=");
+Console.WriteLine(54 & 534);
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
