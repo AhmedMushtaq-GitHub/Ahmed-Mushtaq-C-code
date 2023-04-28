7fedf1ebe4df529fa009f9edd2bec2fd25934410
AhmedMushtaq-GitHub <you@example.com>
2023-04-28 05:44:46 -0700

    Updated

--- Ahmed Mushtaq C# code/Program.cs
+++ Ahmed Mushtaq C# code/Program.cs
@@ -1,164 +1,46 @@
-﻿//C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.
-//C# is used to develop web apps, desktop apps, mobile apps, games and much more.
-Console.WriteLine("Write line is a block level string value");
-Console.Write("Write is a inline string value ");
-Console.WriteLine("Write is a inline string value ");
-Console.WriteLine("Hellow World");
-Console.WriteLine("Write line is a block level string value");
-Console.Write("Write is a inline string value ");
-Console.WriteLine("Write is a inline string value ");
-Console.WriteLine("Hellow World");
-Console.ReadLine();//The only difference is that it does not insert a new line at the end of the output:
-Console.WriteLine("Hello World! ");
-Console.WriteLine("I will print on the same line.");
-// This is a comment
-Console.WriteLine("Hello World!");
-/* The code below will print the words Hello World
-to the screen, and it is amazing */
-Console.WriteLine("Hello World!");
-/*Variables are containers for storing data values.
-In C#, there are different types of variables (defined with different keywords), for example:
-int - stores integers (whole numbers), without decimals, such as 123 or -123
-double - stores floating point numbers, with decimals, such as 19.99 or -19.99
-char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
-string - stores text, such as "Hello World". String values are surrounded by double quotes
-bool - stores values with two states: true or false*/
-int myNum = 5;
-Console.WriteLine(myNum);
-Console.ReadLine();
-double myDoubleNum = 5.99D;
-char myLetter = 'D';
-bool myBool = true;
-string myText = "Hello";
-//This will declare the variable as "constant", which means unchangeable and read-only:
-const int myNums = 15;
-myNum = 20; // error
-string firstName = "John ";
-string lastName = "Doe";
-string fullName = firstName + lastName;
-Console.WriteLine(fullName);
-//To declare more than one variable of the same type, use a comma-separated list:
-int x = 5, y = 6, z = 50;
-Console.WriteLine(x + y + z);
-int a, b, c;
-x = y = z = 50;
-Console.WriteLine(x + y + z);
-Console.WriteLine();
-//Arithmetic Operators
-Console.Write("10 + 9 = ");
-Console.WriteLine(10 + 9); //Plus Operator
-Console.Write("10 - 9 = ");
-Console.WriteLine(10 - 9); //Minus Operator
-Console.Write("10 x 9 = ");
-Console.WriteLine(10 * 9); //Multiply Operator
-Console.Write("10 / 9 = ");
-Console.WriteLine(10 / 9); //Devide Operator
-Console.Write("10 % 9 = ");
-Console.WriteLine(10 % 9); //Modules Operator
-//
-Console.Write("10 + 9 = ");
-Console.WriteLine(10 + 9); //Plus Operator
-Console.Write("10 - 9 = ");
-Console.WriteLine(10 - 9); //Minus Operator
-Console.Write("10 x 9 = ");
-Console.WriteLine(10 * 9); //Multiply Operator
-Console.Write("10 / 9 = ");
-Console.WriteLine(10 / 9); //Devide Operator
-Console.Write("10 % 9 = ");
-Console.WriteLine(10 % 9); //Modules Operator
-//
-Console.Write("10 + 9 = ");
-Console.WriteLine(10 + 9); //Plus Operator
-Console.Write("10 - 9 = ");
-Console.WriteLine(10 - 9); //Minus Operator
-Console.Write("10 x 9 = ");
-Console.WriteLine(10 * 9); //Multiply Operator
-Console.Write("10 / 9 = ");
-Console.WriteLine(10 / 9); //Devide Operator
-Console.Write("10 % 9 = ");
-Console.WriteLine(10 % 9); //Modules Operator
-//
-Console.Write("10 + 9 = ");
-Console.WriteLine(10 + 9); //Plus Operator
-Console.Write("10 - 9 = ");
-Console.WriteLine(10 - 9); //Minus Operator
-Console.Write("10 x 9 = ");
-Console.WriteLine(10 * 9); //Multiply Operator
-Console.Write("10 / 9 = ");
-Console.WriteLine(10 / 9); //Devide Operator
-Console.Write("10 % 9 = ");
-Console.WriteLine(10 % 9); //Modules Operator
-//"AND"   or  "OR" gate //Logical Operators
+﻿//C# Conditions and If Statements
+using System;
 
-Console.WriteLine(534<=234 && 545>=5342);
-Console.WriteLine(4234<=4325 || 435>543);
-Console.WriteLine(645 + 5435);
-Console.WriteLine(42<453 || 43>543);
-Console.ReadLine();
-//
-Console.WriteLine(534 <= 234 && 545 >= 5342);
-Console.WriteLine(4234 <= 4325 || 435 > 543);
-Console.WriteLine(645 + 5435);
-Console.WriteLine(42 < 453 || 43 > 543);
-Console.ReadLine();
-//
-Console.WriteLine(534 <= 234 && 545 >= 5342);
-Console.WriteLine(4234 <= 4325 || 435 > 543);
-Console.WriteLine(645 + 5435);
-Console.WriteLine(42 < 453 || 43 > 543);
-Console.ReadLine();
-//
-Console.WriteLine(534 <= 234 && 545 >= 5342);
-Console.WriteLine(4234 <= 4325 || 435 > 543);
-Console.WriteLine(645 + 5435);
-Console.WriteLine(42 < 453 || 43 > 543);
-Console.ReadLine();
-//Comparison Operators
-//This will Give's Answer in Boolean
-Console.WriteLine(10 > 9); //Means: 10 Greater Than 9
-Console.WriteLine(10 < 9); //Means: 10 Smaller Than 9
-Console.WriteLine(10 == 9); //Means: 10 Equals To 9
-Console.WriteLine(10 >= 9); //Means: 10 Greater Than Or Equal To 9
-Console.WriteLine(10 <= 9); //Means: 10 Smaller Than Or Equal To 9
-Console.WriteLine(10 != 9); //Means: 10 Not Equal To 9
-//Bitwise, Variables and Constants
-Console.Write("3543 | 534 Bitwise is=");
-Console.WriteLine(3543 | 534);
-Console.ReadLine();
-Console.Write("54 & 534 bitwise is=");
-Console.WriteLine(54 & 534);
-Console.ReadLine();
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 24;
+            if (time <= 11)
+            {
+                Console.WriteLine("Good Morning.");
+            }
+            else if (time <= 15)
+            {
+                Console.WriteLine("Good Noon.");
+            }
+            else if (time <= 17)
+            {
+                Console.WriteLine("Good Afternoon.");
+            }
+            else if (time <= 22)
+            {
+                Console.WriteLine("Good evening.");
+            }
+            else
+            {
+                Console.WriteLine("Good Night.");
+            }
+        }
+    }
+    /*class Variables
+    {
+       static void Main () {
+            int x;
+            int y;
+            x = 67;
+            y = x / 6;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }*/
+}
